5ebf013770324e71982b81b6f05f8b844647183f
Eljan2001 <eljan.abbaszada.std@bhos.edu.az>
2022-09-30 13:59:01 +0400

    make it better

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,44 +2,48 @@ package org.example;
 
 class myownclass {
     static int addNumbers(int a, int b) {
-        return a+b;
+        int sum = a + b;
+        return sum;
     }
 
     public static int subtractNumbers(int a, int b) {
-        return a-b;
+        int subt = a - b;
+        return subt;
     }
 
     public static int multiplyNumbers(int a, int b) {
-        return a * b;
+        int mult = a * b;
+        return mult;
     }
 
     public static int divideNumbers(int a, int b) {
-        return a/b;
+        int div = a / b;
+        return div;
     }
 
     public static int square(int num) {
-
-        return num * num;
+        int sq = num*num;
+        return sq;
     }
 
     public static int cube(int num) {
-
-        return num * num * num;
+        int cb = num*num*num;
+        return cb;
     }
 
     public static int quad(int num) {
-
-        return num * num * num * num;
+        int qd = num*num*num*num;
+        return qd;
     }
 
     public static int fifthpower(int num) {
-
-        return num * num * num * num * num;
+        int ftpw = num*num*num*num*num;
+        return ftpw;
     }
 
     public static int sixthpower(int num) {
-
-        return num * num * num * num * num * num;
+        int sxpw = num * num * num * num * num * num;
+        return sxpw;
     }
 
     public static int printitself(int num) {
